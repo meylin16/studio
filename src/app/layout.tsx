a716721c0a57ae128af9dc03f42c6d2e7395fbbd
Carlos Daniel Gil <cardani2429.cdc@gmail.com>
2025-04-28 02:38:31 +0000

    Mira que quiero exportar el codigo y me aparece el siguiente error, podrias ayudarme a corrgir ?
    In HTML, whitespace text nodes cannot be a child of <html>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    This will cause a hydration error.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es"> {/* Set language to Spanish */}
+    <html lang="es"> {/* Ensure no extra whitespace or comments here */}
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         <Toaster /> {/* Add Toaster here */}
